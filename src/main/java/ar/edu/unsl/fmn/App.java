8e8b23d9c032c14a136b5e706e8cd7fd536fc30d
Daniel Kwist <kwistdaniel@gmail.com>
2022-09-20 18:00:28 -0300

    fix

--- src/main/java/ar/edu/unsl/fmn/App.java
+++ src/main/java/ar/edu/unsl/fmn/App.java
@@ -20,16 +20,13 @@ public class App {
 
     private static final double SIMULATION_LENGHT = 40320d;
     //private static final double SIMULATION_LENGHT = 100d;
+
     public static void main(String[] args) {
-        //Inicializacion
         Engine engine = new AirportSim(
                 SIMULATION_LENGHT,
                 ScenarioBuilder.OneServerOneQueue(),
                 new UniqueServerSelectionPolicy(),
                 new CustomRandomizer());
-        //ver el mensaje de abajo completo
-        //Para ver la cant total de entidades atendidas, tendria que contar en cada server las que atendio y devolver la suma de esas, en la policy entonces deberia tener algo que me retorne todos los servers que tengo la cantidad que atendieron o algo asi
-        //Arrival y EOS tienen mismo order en el constructor, se trataba primero la salida y despues la entrada?
         engine.execute();
         engine.generateReport();
     }
